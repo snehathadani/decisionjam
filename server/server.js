510acf3da7692bd1fb0b5c2c497ca4f01c4975ec
Sneha Thadani <snehacopper@gmail.com>
2018-05-02 22:31:31 -0700

    added post request to add user

--- server/server.js
+++ server/server.js
@@ -1,34 +1,52 @@
+const bodyParser = require('body-parser');
 const express = require ('express');
 const server = express();
 const mongoose = require('mongoose');
 const User = require('./db/UserModel.js');
+const cors = require('cors');
+
+const STATUS_USER_ERROR = 422;
+const STATUS_SERVER_ERROR = 500;
+const STATUS_OKAY = 200;
+
+server.use(cors());
+server.use(bodyParser.json());
 
 
 server.get ('/', function (req, res) {
-    res.status(200).json ({message: 'API running'});
+   res.status(200).json ({message: 'API running'});
 });
 
-server.get ('/users', function (req, res) {
-   User.find({},(err, users)=> {
-       if (err) {
+server.get ('/api/users', function (req, res) {
+  User.find({},(err, users)=> {
+        if (err) {
+        res.status(STATUS_USER_ERROR).json({error:'Could not find the user.'});
+        } else {
+            res.status(200).json(users);
+        }
+  })
+});
 
-       } else {
-           res.status(200).json(users);
-       }
-   })
+server.post('/api/users/adduser', function(req, res) {
+    const newUser = new User (req.body);
+    //check the user contains all required data
+    newUser.save((err, user) => {
+        if(err) {
+            res.status(STATUS_USER_ERROR).json({error: "Error while adding"});
+        } else {
+            res.status(200).json(user);
+        }
+    })
 });
 
 mongoose.Promise = global.Promise;
 const connect = mongoose.connect(
-    'mongodb://sneha.thadani:decisionjam@ds163769.mlab.com:63769/decisionjam');
+   'mongodb://sneha.thadani:decisionjam@ds163769.mlab.com:63769/decisionjam');
 
 connect.then(()=> {
-    const port= 8000;
-    server.listen(port);
-    console.log(`Server Listening on ${port}`);
-
+   const port= 8000;
+   server.listen(port);
+   console.log(`Server Listening on ${port}`);
 }, (err)=> {
-    console.log('could not connect to MongoDB');
+   console.log('could not connect to MongoDB');
 });
-
-
