7f0859cc0fbaa7c5d0dd2c19757b4f3109c1207d
Sneha Thadani <snehacopper@gmail.com>
2018-05-05 10:35:39 -0700

    basic decision page no css

---
+++ frontend/src/components/question/Question.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./Question.css";
+
+const Question = () => {
+    return(
+        <div>
+        <div>
+            <label> Question or decision </label>   
+            <input type = "text"/>
+        </div>    
+            <button> Create Question </button>
+        </div>
+    )
+};
+
+export default Question;
