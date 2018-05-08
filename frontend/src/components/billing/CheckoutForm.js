f0cc74e9344bc31c8bf84a4779844c21117a753f
Jamie Domingo <jaybmw@aol.com>
2018-05-07 23:58:04 -0700

    Added billing routes to create user and subscription

--- frontend/src/components/billing/CheckoutForm.js
+++ frontend/src/components/billing/CheckoutForm.js
@@ -19,17 +19,15 @@ class CheckoutForm extends Component {
 
     this.props.stripe.createToken({user: this.state.name})
     .then(({token}) => {
-
     const postData = {
       selectedOption: this.state.selectedOption,
       token: token,
     }
       axios.post(`${ROOT_URL}/billing`, { postData })
-      console.log('postData:', postData);
+      // console.log('postData:', postData);
+      // console.log(`You chose ${this.state.selectedOption}`);
       console.log('Received Stripe token:', token);
     });
-
-    console.log(`You chose ${this.state.selectedOption}`);
 
   }
 
@@ -43,15 +41,20 @@ class CheckoutForm extends Component {
   render() {
     return (
         <form onSubmit={this.handleSubmit}>
+           
             <CardSection />
             <div className="radiobuttons">
             <label> 
-              <input type="radio" value="option1" checked={this.state.selectedOption === 'option1'} onChange={this.handleOptionChange}/>
-              Subscription
+              <input type="radio" value="HalfYearly" checked={this.state.selectedOption === 'HalfYearly'} onChange={this.handleOptionChange}/>
+              HalfYearly
             </label>
             <label> 
-              <input type="radio" value="option2" checked={this.state.selectedOption === 'option2'} onChange={this.handleOptionChange}/>
-              Pay As You Go
+              <input type="radio" value="Monthly" checked={this.state.selectedOption === 'Monthly'} onChange={this.handleOptionChange}/>
+              Monthly
+            </label>
+            <label> 
+              <input type="radio" value="PerDecision" checked={this.state.selectedOption === 'PerDecision'} onChange={this.handleOptionChange}/>
+              Per Decision
             </label>
             </div>
             <button>Confirm order</button>
--- server/server.js
+++ server/server.js
@@ -4,6 +4,9 @@ const server = express();
 const mongoose = require('mongoose');
 const User = require('./db/UserModel.js');
 const cors = require('cors');
+// const jwt = require('jwt-simple');
+// const passport = require('passport');
+// const config = require('./config/passport.js');
 
 const STATUS_USER_ERROR = 422;
 const STATUS_SERVER_ERROR = 500;
@@ -11,16 +14,14 @@ const STATUS_OKAY = 200;
 
 server.use(cors());
 server.use(bodyParser.json());
+// server.use(passport.initialize());
+// pass passport for configuration
+// require('./config/passport')(passport);
 
 
-server.post ('/billing', function (req, res) {
-    console.log('server: card charge button clicked');
-    console.log('req.body.postData.token.id', req.body.postData.token.id);
-    console.log('req.body.postData.selectedOption', req.body.postData.selectedOption);
+const payments = require('./Payments.js');
+payments(server);
 
-    res.status(200).json ({message: 'Card charged'});
- });
- 
 
 
 server.get ('/', function (req, res) {
@@ -40,18 +41,70 @@ server.get ('/api/users', function (req, res) {
 server.post('/api/users/adduser', function(req, res) {
     const newUser = new User (req.body);
     //check the user contains all required data
+    if(!newUser.username || !newUser.password || !newUser.email) {
+        res.status(400).json('missing required info');
+        return;
+    }
     newUser.save((err, user) => {
         if(err) {
-            res.status(STATUS_USER_ERROR).json({error: "Error while adding"});
+            res.status(STATUS_USER_ERROR).json({error: "Error while adding", err});
         } else {
             res.status(200).json(user);
         }
     })
-}); 
+});
+
+//gotta convert ugly callback code to beautiful promises
+//http://erikaybar.name/using-es6-promises-with-mongoosejs-queries/
+// route to authenticate a user (POST http://localhost:8080/api/login)
+server.post('/api/login', function(req, res) {
+    User.findOne({
+      $or: [{'email': req.body.emailOrUsername}, {'username': req.body.emailOrUsername}]
+    }, function(err, user) {
+      if (err) throw err;
+   
+      if (!user) {
+        res.send({success: false, msg: 'Authentication failed. User not found.'});
+      } else {
+        // check if password matches
+        console.log (user.password, req.body.password);
+        user.comparePassword(req.body.password, function (err, isMatch) {
+          console.log(isMatch);
+          if (isMatch && !err) {
+            // if user is found and password is right create a token
+            var token = jwt.encode(user, 'cs5Rocks');
+            // return the information including token as JSON
+            res.json({success: true, token: 'JWT ' + token});
+          } else {
+            res.send({success: false, msg: 'Authentication failed. Wrong password.', err });
+          }
+        });
+      }
+    });
+  });
+
+  /* Handle Logout */
+  // nice to have, need to refresh the session on each authorised route so the user
+  
+  //see last comment https://stackoverflow.com/questions/45541182/passport-req-logout-function-not-working 
+server.get('/api/logout', function(req, res) {
+  console.log("I am Logout")
+  req.logout(); 
+  res.status(200).redirect('/');
+});
+
+
+  
+  //how to setup routes that need auth as well as test it on postman
+  //https://jonathanmh.com/express-passport-json-web-token-jwt-authentication-beginners/
+  // server.get('/api/routeThatNeedsJWTToken', passport.authenticate('jwt', { session: false }), function(req, res){
+  //   res.json("Success! You can not see this without a token");
+  // });
 
 mongoose.Promise = global.Promise;
 const connect = mongoose.connect(
-   'mongodb://localhost/decisionjam)');
+   'mongodb://localhost/decisionjam');
+   //mongodb://sneha.thadani:decisionjam@ds163769.mlab.com:63769/decisionjam
 
 connect.then(()=> {
    const port= 8000;
