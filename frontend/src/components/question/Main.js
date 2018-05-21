3cef2873f99825593dd494caff35587e2108e07f
Jamie Domingo <jaybmw@aol.com>
2018-05-20 19:17:53 -0700

    fixed redirect in question page for auth

--- frontend/src/components/question/Main.js
+++ frontend/src/components/question/Main.js
@@ -1,29 +1,29 @@
 import React, { Component } from "react";
 import "./Main.css";
+import axios from "axios";
+import { Redirect } from "react-router-dom";
 
 class Main extends Component {
   state = {
-    decisionCode: ""
+    decisionCode: "",
+    redirect: false,
+    result: false,
+    jwtToken: localStorage.getItem("token")
   };
 
-  render() {
-    return (
-      <div className="main-wrapper">
-        <label className="luckyyou"> "Lucky You" </label>
-        <div className="enter-text">Enter Decision Code</div>
-        <div className="main-input-wrapper">
-          <input
-            className="main-input"
-            type="text"
-            value={this.state.decisionCode}
-            onChange={this.setDecisionCode}
-          />
-        </div>
-        <div>
-          <button onClick={this.joinDecision}> Join </button>
-        </div>
-      </div>
-    );
+  componentDidMount() {
+    const headers = {
+      "Content-Type": "application/json",
+      Authorization: this.state.jwtToken
+    };
+    axios
+      .get(`http://localhost:8000/api/routeThatNeedsJWTToken`, { headers })
+      .then(res => {
+        this.setState({ result: true });
+      })
+      .catch(error => {
+        this.setState({ result: true, redirect: true });
+      });
   }
 
   setDecisionCode = event => {
@@ -35,5 +35,33 @@ class Main extends Component {
       "/decision/decisionCode/" + this.state.decisionCode
     );
   };
+
+  render() {
+    if (this.state.result) {
+      if (this.state.redirect === true) {
+        return <Redirect to={"/signup"} />;
+      } else {
+        return (
+          <div className="main-wrapper">
+            <label className="luckyyou"> "Lucky You" </label>
+            <div className="enter-text">Enter Decision Code</div>
+            <div className="main-input-wrapper">
+              <input
+                className="main-input"
+                type="text"
+                value={this.state.decisionCode}
+                onChange={this.setDecisionCode}
+              />
+            </div>
+            <div>
+              <button onClick={this.joinDecision}> Join </button>
+            </div>
+          </div>
+        );
+      }
+    } else {
+      return null;
+    }
+  }
 }
 export default Main;
--- frontend/src/components/question/Question.js
+++ frontend/src/components/question/Question.js
@@ -6,15 +6,55 @@ import { Redirect } from "react-router-dom";
 class Question extends Component {
   state = {
     decisionText: "",
-    jwtToken: localStorage.getItem("token"),
     decisionCode: "",
     redirect: false,
-    result: false
+    result: false,
+    jwtToken: localStorage.getItem("token"),
+
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: localStorage.getItem("token")
+    }
+  };
+
+  // check auth to view page
+  componentDidMount() {
+    const headers = this.state.headers;
+
+    axios
+      .get(`http://localhost:8000/api/routeThatNeedsJWTToken`, { headers })
+      .then(res => {
+        this.setState({ result: true });
+      })
+      .catch(error => {
+        this.setState({ result: true, redirect: true });
+      });
+  }
+
+  setDecisionText = event => {
+    this.setState({ decisionText: event.target.value });
+  };
+
+  createQuestion = event => {
+    // console.log("Sending " + this.state.decisionText);
+    const postData = {
+      decisionText: this.state.decisionText
+    };
+    const headers = this.state.headers;
+    axios
+      .post("http://localhost:8000/api/decision/create", postData, { headers })
+      .then(decision => {
+        console.log("decision", decision);
+        this.setState({ decisionCode: decision.data.decision.decisionCode });
+        this.props.history.push(
+          "/decision/decisionCode/" + this.state.decisionCode
+        );
+      })
+      .catch(error => console.log("Got error " + error.response.data.error));
   };
 
   render() {
-    console.log(this.state.jwtToken);
-
+    // console.log("this.state", this.state);
     if (this.state.result) {
       if (this.state.redirect === true) {
         return <Redirect to={"/signup"} />;
@@ -40,56 +80,6 @@ class Question extends Component {
       return null;
     }
   }
-
-  componentDidMount() {
-    const headers = {
-      "Content-Type": "application/json",
-      Authorization: this.state.jwtToken
-    };
-    axios
-      .get(`http://localhost:8000/api/routeThatNeedsJWTToken`, { headers })
-      .then(res => {
-        console.log("res", res);
-        this.setState({
-          result: true
-        });
-      })
-      .catch(error => {
-        console.log("error", error.response.data);
-
-        // console.log("error", error.response.data.error);
-        this.setState({
-          result: true,
-          redirect: true
-        });
-      });
-  }
-
-  setDecisionText = event => {
-    this.setState({
-      decisionText: event.target.value
-    });
-  };
-
-  createQuestion = event => {
-    console.log("Sending " + this.state.decisionText);
-    const postData = {
-      decisionText: this.state.decisionText
-    };
-    console.log("decisionText", this.state.decisionText);
-
-    axios
-      .post("http://localhost:8000/api/decision/create", postData)
-      .then(decision => {
-        //TODO redirect to the next page with the decsion id here
-        console.log("decision", decision);
-        this.setState({ decisionCode: decision.data.decision.decisionCode });
-        this.props.history.push(
-          "/decision/decisionCode/" + this.state.decisionCode
-        );
-      })
-      .catch(error => console.log("Got error " + error.response.data.error));
-  };
 }
 
 export default Question;
